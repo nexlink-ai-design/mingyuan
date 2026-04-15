0aaf078cf02ab37559860467a7ff10a14017fc32
mason <biyong@gmaol.com>
2026-04-16 02:45:42 +0800

    test add

--- components/companions/MobileBottomNav.tsx
+++ components/companions/MobileBottomNav.tsx
@@ -26,8 +26,8 @@ function MobileNavItem({ icon: Icon, label, active = false, badge }: MobileNavIt
 export default function MobileBottomNav() {
   return (
     <nav className="lg:hidden fixed bottom-0 left-0 right-0 bg-white border-t border-gray-100 flex justify-around items-center h-16 px-2 safe-bottom z-40">
-      <MobileNavItem icon={Home} label="陪玩" active />
-      <MobileNavItem icon={Compass} label="悦享" />
+      <MobileNavItem icon={Home} label="悦享" active />
+      <MobileNavItem icon={Compass} label="选妃" />
       <MobileNavItem icon={ClipboardList} label="订单" badge="9" />
       <MobileNavItem icon={CircleUser} label="我的" />
     </nav>
